4510770b5d3061646ef8f4b28a5c9f1310184c7d
Александр <lion2head@gmail.com>
2016-07-13 23:41:27 +0400

    remarks

--- exercise/solution.js
+++ exercise/solution.js
@@ -1,14 +1,14 @@
 // BEGIN (write your solution here)
-const akkerman = (m, n) => {
-  if (m < 0 || n < 0) return undefined;
+const ackermann = (m, n) => {
+  if (m < 0 || n < 0) return;
   if (m === 0) {
     return n + 1;
   } else if (n === 0 && m > 0) {
-    return akkerman(m - 1, 1);
+    return ackermann(m - 1, 1);
   }
 
-  return akkerman(m - 1, akkerman(m, n - 1));
+  return ackermann(m - 1, ackermann(m, n - 1));
 };
 
-export default akkerman;
+export default ackermann;
 // END
--- exercise/test.js
+++ exercise/test.js
@@ -1,18 +1,18 @@
 /* global define, it, describe */
 
 import assert from 'assert';
-import akkerman from './solution';
+import ackermann from './solution';
 
-describe('akkerman', () => {
+describe('ackermann', () => {
   it('should work', () => {
-    assert.equal(1, akkerman(0, 0));
-    assert.equal(3, akkerman(1, 1));
-    assert.equal(4, akkerman(1, 2));
-    assert.equal(5, akkerman(2, 1));
-    assert.equal(9, akkerman(2, 3));
+    assert.equal(ackermann(0, 0), 1);
+    assert.equal(ackermann(1, 1), 3);
+    assert.equal(ackermann(1, 2), 4);
+    assert.equal(ackermann(2, 1), 5);
+    assert.equal(ackermann(2, 3), 9);
   });
   it('bad value', () => {
-    assert.equal(undefined, akkerman(-1, 1));
-    assert.equal(undefined, akkerman(1, -1));
+    assert.equal(ackermann(-1, 1), undefined);
+    assert.equal(ackermann(1, -1), undefined);
   });
 });
